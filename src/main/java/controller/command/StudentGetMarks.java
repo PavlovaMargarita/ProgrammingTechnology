534217aff41691fb66e8f33dc123afe402d0977f
PavlovaMargarita <xomrita@gmail.com>
2014-12-08 19:19:38 +0300

    example: connect to database

--- src/main/java/controller/command/StudentGetMarks.java
+++ src/main/java/controller/command/StudentGetMarks.java
@@ -1,7 +1,12 @@
 package controller.command;
 
+import param.Params;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * Created by marharyta.pavlova on 08-Dec-14.
@@ -10,5 +15,18 @@ public class StudentGetMarks implements Command {
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
         return null;
+    }
+
+    private void connectToDb(){
+        try {
+            Class.forName(Params.bundle.getString("urlDriver"));
+            Connection connect = DriverManager.getConnection(Params.bundle.getString("urlDB"),
+                    Params.bundle.getString("userDB"),Params.bundle.getString("passwordDB"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/param/Params.java
+++ src/main/java/param/Params.java
@@ -1,10 +1,13 @@
 package param;
 
+import java.util.ResourceBundle;
+
 /**
  * Created by marharyta.pavlova on 08-Dec-14.
  */
 public class Params {
 
+    public static ResourceBundle bundle = ResourceBundle.getBundle("database");
     public static final String METHOD = "method";
     public static final String GET_STUDENT_MARKS = "getStudentMarks";
 }
